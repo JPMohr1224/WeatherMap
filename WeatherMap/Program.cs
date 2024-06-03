afc0c4cbda3329c02b3722612dc484711fc4924d
JP Mohr <James.Mohr2.0@hotmail.com>
2024-06-03 15:59:07 -0400

    WeatherMapAPI

--- WeatherMap/Program.cs
+++ WeatherMap/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json.Linq;
+
+
+
+var client = new HttpClient();
+
+//var city = "Norfolk";
+//var state = "Va";
+//var country = "USA";
+
+var appsettingsText = File.ReadAllText("appsettings.json");
+var apiKey = JObject.Parse(appsettingsText)["key"].ToString();
+
+
+var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q=Norfolk,Va,USA&appid={apiKey}&units=imperial";
+var response = client.GetStringAsync(weatherURL).Result;
+
+Console.WriteLine(JObject.Parse(response)["main"]["temp"].ToString());
 
